25fdce2d31eae5885299e6ef400d1cdcb0a2b236
francescoP02 <file.partipilo@gmail.com>
2022-04-11 13:26:50 +0200

    Done console script

--- Console/js/script.js
+++ Console/js/script.js
@@ -1,1 +1,29 @@
-alert(`Hello!`);
+// Stampare nella pagina i numeri da 0 a 100, se è multiplo di 3 stampa Fizz, se è multiplo di 5 stampa Buzz, se è multiplo di 3 e 5 stampa FizzBuzz
+
+// Stampa nella console
+for (let i = 1; i <= 100; i++) {
+
+    // Definire se è multiplo di 3, di 5 o di entrambi
+
+    let multiple;
+
+    if (i % 3 === 0 && i % 5 === 0) {
+        multiple = "FizzBuzz";
+    }
+
+    else if (i % 3 === 0) {
+        multiple = "Fizz";
+    }
+
+    else if (i % 5 === 0) {
+        multiple = "Buzz";
+    }
+
+    else {
+        multiple = i;
+    }
+
+    // controllo
+
+    console.log(multiple);
+}
